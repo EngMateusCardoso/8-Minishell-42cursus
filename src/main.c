20f7d033b72706c668d83521872606be9acdaefc
EngMateusCardoso <mateusoliver490@gmail.com>
2022-12-09 17:44:36 -0300

    readline tests

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: matcardo <matcardo@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/15 06:34:38 by matcardo          #+#    #+#             */
-/*   Updated: 2022/12/09 17:13:04 by matcardo         ###   ########.fr       */
+/*   Updated: 2022/12/09 17:40:28 by matcardo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,11 @@
 
 int main()
 {
-    ft_putstr_fd("Hello World!", 1);
+    char *prompt_input;
+    ft_putstr_fd("Hello minishell!", 1);
+    while (!ft_strncmp(prompt_input, "exit", 5))
+    {
+        prompt_input = readline("minishell$ ");
+    }
     return 0;
 }
--- src/minishell.h
+++ src/minishell.h
@@ -6,13 +6,15 @@
 /*   By: matcardo <matcardo@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/09 16:52:56 by matcardo          #+#    #+#             */
-/*   Updated: 2022/12/09 16:54:36 by matcardo         ###   ########.fr       */
+/*   Updated: 2022/12/09 17:42:03 by matcardo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MINISHELL_H
 # define MINISHELL_H
 
-# include "libft.h"
+# include <readline/readline.h>
+# include <stdio.h>
+# include "../libraries/libft/libft.h"
 
 #endif
