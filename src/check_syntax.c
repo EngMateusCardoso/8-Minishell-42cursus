a2d288a19bf1951383fc80951a7d5821c00fc59e
Thayná Habeck <thaynahabeck@gmail.com>
2023-02-13 17:45:53 -0300

    builtin cd

--- src/check_syntax.c
+++ src/check_syntax.c
@@ -6,7 +6,7 @@
 /*   By: thabeck- <thabeck-@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/23 19:59:39 by matcardo          #+#    #+#             */
-/*   Updated: 2023/02/12 22:12:55 by thabeck-         ###   ########.fr       */
+/*   Updated: 2023/02/13 16:57:54 by thabeck-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,6 +21,27 @@ short int  is_redirection_or_pipe(char *token)
         ft_strncmp(token, "<<", 2) == 0 || ft_strncmp(token, "||", 2) == 0)
         return (1);
     return (0);
+}
+
+void	error_msg(char *cmd, char *msg, int status)
+{
+	ft_putstr_fd("minishell: ", 2);
+	ft_putstr_fd(cmd, 2);
+	ft_putstr_fd(": ", 2);
+	ft_putstr_fd(msg, 2);
+	ft_putstr_fd("\n", 2);
+	g_data.exit_code = status;
+}
+
+void	error_handler(char *str1, char *str2, int status, char *cmd)
+{
+	ft_putstr_fd("minishell: ", 2);
+	ft_putstr_fd(cmd, 2);
+	ft_putstr_fd(": ", 2);
+	ft_putstr_fd(str1, 2);
+	ft_putstr_fd(str2, 2);
+	perror("");
+	g_data.exit_code = status;
 }
 
 short int    syntax_error(char **command_tokens, char *token)
