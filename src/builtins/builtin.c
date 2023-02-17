bee48c399ec008eeebb3f360850e06970d892ae8
Thayná Habeck <thaynahabeck@gmail.com>
2023-02-17 18:36:35 -0300

    reorganized builtins

--- src/builtins/builtin.c
+++ src/builtins/builtin.c
@@ -6,11 +6,45 @@
 /*   By: thabeck- <thabeck-@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/10 20:32:35 by matcardo          #+#    #+#             */
-/*   Updated: 2023/02/15 21:08:13 by thabeck-         ###   ########.fr       */
+/*   Updated: 2023/02/17 18:35:45 by thabeck-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../minishell.h"
+
+void	execute_builtin(char **command, int isfork)
+{
+	if (isfork)
+	{
+		if (!ft_strncmp(command[0], "echo", 5))
+			echo_builtin(command);
+		else if (!ft_strncmp(command[0], "pwd", 4))
+			pwd_builtin();
+		else if (!ft_strncmp(command[0], "env", 4))
+			env_builtin();
+	}
+	else
+	{
+		if (!ft_strncmp(command[0], "cd", 3))
+			cd_builtin(command);
+		else if (!ft_strncmp(command[0], "export", 7))
+			export_builtin(command);
+		else if (!ft_strncmp(command[0], "unset", 6))
+			unset_builtin(command);
+		else if (!ft_strncmp(command[0], "exit", 5))
+			exit_builtin(command);
+	}
+}
+
+int	is_builtin(char *command)
+{
+	if (!ft_strncmp(command, "echo", 5) || !ft_strncmp(command, "cd", 3) || \
+		!ft_strncmp(command, "pwd", 4) || !ft_strncmp(command, "export", 7) \
+		|| !ft_strncmp(command, "unset", 6) || !ft_strncmp(command, "env", 4) \
+		|| !ft_strncmp(command, "exit", 5))
+		return (1);
+	return (0);
+}
 
 int	check_identifier(char *var)
 {
@@ -40,24 +74,6 @@ int	check_identifier(char *var)
 	return (1);
 }
 
-char	*clear_quotes(char *var)
-{
-	char	*cmd;
-	int		i;
-
-	cmd = NULL;
-	i = 0;
-	while (var[i])
-	{
-		if ((var[i] != '\'') && (var[i] != '\"'))
-		{
-			cmd = ft_straddchar(cmd, var[i]);
-		}
-		i++;
-	}
-	return (cmd);
-}
-
 void	identifier_error(char *cmd, char **value)
 {
 	char	*tmp;
@@ -75,36 +91,20 @@ void	identifier_error(char *cmd, char **value)
 		error_msg(cmd, ": `': not a valid identifier", 1);
 }
 
-int	is_builtin(char *command)
+char	*clear_quotes(char *var)
 {
-	if (!ft_strncmp(command, "echo", 5) || !ft_strncmp(command, "cd", 3) || \
-		!ft_strncmp(command, "pwd", 4) || !ft_strncmp(command, "export", 7) \
-		|| !ft_strncmp(command, "unset", 6) || !ft_strncmp(command, "env", 4) \
-		|| !ft_strncmp(command, "exit", 5))
-		return (1);
-	return (0);
-}
+	char	*cmd;
+	int		i;
 
-void	execute_builtin(char **command, int isfork)
-{
-	if (isfork)
+	cmd = NULL;
+	i = 0;
+	while (var[i])
 	{
-		if (!ft_strncmp(command[0], "echo", 5))
-			echo_builtin(command);
-		else if (!ft_strncmp(command[0], "pwd", 4))
-			pwd_builtin();
-		else if (!ft_strncmp(command[0], "env", 4))
-			env_builtin();
+		if ((var[i] != '\'') && (var[i] != '\"'))
+		{
+			cmd = ft_straddchar(cmd, var[i]);
+		}
+		i++;
 	}
-	else
-	{
-		if (!ft_strncmp(command[0], "cd", 3))
-			cd_builtin(command);
-		else if (!ft_strncmp(command[0], "export", 7))
-			export_builtin(command);
-		else if (!ft_strncmp(command[0], "unset", 6))
-			unset_builtin(command);
-		else if (!ft_strncmp(command[0], "exit", 5))
-			exit_builtin(command);
-	}
+	return (cmd);
 }
